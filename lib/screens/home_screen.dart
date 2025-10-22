11bf1f0bda98a103a9a5a2669cdfa71c3bc5ff80
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-10-22 15:38:35 +0545

    added: required assets for the app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,12 +11,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Screen"),
-      ),
-      body: const Center(
-        child: Text("Welcome to A R Y A!"),
-      ),
+      appBar: AppBar(title: const Text("Home Screen")),
+      body: const Center(child: Text("Welcome to A R Y A!")),
     );
   }
 }
